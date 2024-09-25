7b5055d1e2f275532cfb7732ea7768ef42e1dc1d
Luiz Claudio <luiz.claudiolc@outlook.com>
2024-09-25 18:28:47 -0300

    fix: test de enviroments

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: true,
+  endpoint: {
+    auth: {
+      create: 'https://api-app-todo.onrender.com/api/v2/auth/create',
+      login: 'https://api-app-todo.onrender.com/api/v2/auth/login',
+    },
+    task: 'https://api-app-todo.onrender.com/api/v2/task',
+  },
+};
