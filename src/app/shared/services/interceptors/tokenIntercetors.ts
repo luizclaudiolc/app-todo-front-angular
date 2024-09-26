39b38e895347b72d88e34c142c66a5bd149d0fbf
Luiz Claudio <luiz.claudiolc@outlook.com>
2024-09-26 10:56:42 -0300

    fix: muda para env

--- src/app/shared/services/interceptors/tokenIntercetors.ts
+++ src/app/shared/services/interceptors/tokenIntercetors.ts
@@ -20,10 +20,10 @@ export class TokenInterceptor implements HttpInterceptor {
   ): Observable<HttpEvent<any>> {
     const token = localStorage.getItem('token');
     const requestUrl: Array<any> = request.url.split('/');
-    const apiUrl: Array<any> = environment.AUTH_LOGIN.split('/');
+    const apiUrl: Array<any> = process.env['AUTH_LOGIN']!.split('/');
     console.log({ token, requestUrl: request.url });
 
-    if (token && token && request.url.startsWith(environment.AUTH_LOGIN)) {
+    if (token && token && request.url.startsWith(process.env['AUTH_LOGIN']!)) {
       request = request.clone({
         setHeaders: {
           Authorization: `Bearer ${token}`,
