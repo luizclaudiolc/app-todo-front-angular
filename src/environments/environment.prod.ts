e37758c13de1312f69815012dee92626c8d02e9c
Luiz Claudio <luiz.claudiolc@outlook.com>
2024-09-26 10:13:28 -0300

    fix: muda variaiveis de ambiente para vercel

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,13 @@
 export const environment = {
   production: true,
-  endpoint: {
+  AUTH_CREATE: 'https://api-app-todo.onrender.com/api/v2/auth/create',
+  AUTH_LOGIN: 'https://api-app-todo.onrender.com/api/v2/auth/login',
+  TASK: 'https://api-app-todo.onrender.com/api/v2/task',
+  /* endpoint: {
     auth: {
       create: 'https://api-app-todo.onrender.com/api/v2/auth/create',
       login: 'https://api-app-todo.onrender.com/api/v2/auth/login',
     },
     task: 'https://api-app-todo.onrender.com/api/v2/task',
-  },
+  }, */
 };
