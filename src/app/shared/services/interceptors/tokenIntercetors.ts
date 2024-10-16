0e79987442802fa639aa37e959b8843fd96c1a6e
Luiz Claudio <luiz.claudiolc@outlook.com>
2024-10-16 08:30:17 -0300

    fix: change interceptador

--- src/app/shared/services/interceptors/tokenIntercetors.ts
+++ src/app/shared/services/interceptors/tokenIntercetors.ts
@@ -80,13 +80,12 @@ export class TokenInterceptor implements HttpInterceptor {
     const refreshToken = localStorage.getItem("refreshToken");
 
     if (accessToken && request.url.startsWith(environment.TASK)) {
-      request = this.addTokenToRequest(request, accessToken);
+      request = this.addToken(request, accessToken);
 
       return next.handle(request).pipe(
         catchError((error: HttpErrorResponse) => {
-          // Verifica se o erro é de token expirado (401) e se há um refreshToken disponível
           if (error.status === 401 && refreshToken) {
-            return this.handle401Error(request, next, refreshToken);
+            return this.handleRefreshToken(request, next, refreshToken);
           } else {
             this.authService.logout();
             return throwError(
@@ -100,40 +99,30 @@ export class TokenInterceptor implements HttpInterceptor {
     }
   }
 
-  // Método para adicionar o token ao cabeçalho da requisição
-  private addTokenToRequest(
-    request: HttpRequest<any>,
-    token: string,
-  ): HttpRequest<any> {
+  private addToken(request: HttpRequest<any>, token: string): HttpRequest<any> {
     return request.clone({
       setHeaders: {
         Authorization: `Bearer ${token}`,
-        token,
+        token: token,
       },
     });
   }
 
-  // Método para lidar com o erro 401 e tentar o refresh do token
-  private handle401Error(
+  private handleRefreshToken(
     request: HttpRequest<any>,
     next: HttpHandler,
     refreshToken: string,
   ): Observable<HttpEvent<any>> {
     return this.authService.refreshToken(refreshToken).pipe(
       switchMap(({ accessToken, refreshToken }) => {
-        // Salva os novos tokens no localStorage
         localStorage.setItem("accessToken", accessToken);
         localStorage.setItem("refreshToken", refreshToken);
-
-        // Clona a requisição original com o novo token
-        request = this.addTokenToRequest(request, accessToken);
-        return next.handle(request);
+        return next.handle(this.addToken(request, accessToken));
       }),
       catchError((refreshError) => {
-        // Se o refresh falhar, faz o logout
         this.authService.logout();
         return throwError(
-          () => new Error(refreshError.message || "Erro ao renovar token"),
+          () => new Error(refreshError.message || "Erro desconhecido"),
         );
       }),
     );
